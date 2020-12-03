16bc9223269138eacda29693d043a3c7303882d3
shadz22 <shadz22@yahoo.co.uk>
2020-12-03 13:17:57 +0000

    Rendering the fetched recipe's cooking time and the clock icon to the page

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,5 @@
+import icons from 'url:../img/icons.svg';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const showRecipe = async function() {
@@ -18,15 +20,27 @@ const showRecipe = async function() {
       image: recipe.image_url,
       ingredients: recipe.ingredients
     };
-    console.log(recipe);
+    // console.log(recipe);
+    console.log(icons);
 
     const markup = `
     <figure class="recipe__fig">
-          <img src="${recipe.image}" alt="Tomato" class="recipe__img" />
-          <h1 class="recipe__title">
-            <span>${recipe.title}</span>
-          </h1>
-        </figure>
+      <img src="${recipe.image}" alt="Tomato" class="recipe__img" />
+      <h1 class="recipe__title">
+        <span>${recipe.title}</span>
+      </h1>
+    </figure>
+
+    <div class="recipe__details">
+      <div class="recipe__info">
+        <svg class="recipe__info-icon">
+          <use href="${icons}#icon-clock"></use>
+        </svg>
+        <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
+        <span class="recipe__info-text">minutes</span>
+      </div>
+    </div>
+
         `;
     recipeContainer.insertAdjacentHTML('afterbegin', markup);
 

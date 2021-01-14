2f13345f3fdfb7d57b8a68a00d11a33b1c23246b
shadz22 <shadz22@yahoo.co.uk>
2021-01-14 18:39:50 +0000

    Use Fraction for showing ingredients' quantity

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,4 +1,5 @@
 import icons from 'url:../../img/icons.svg';
+import { Fraction } from 'fractional';
 
 class RecipeView {
   #parentElement = document.querySelector('.recipe');
@@ -75,7 +76,7 @@ class RecipeView {
                 <svg class="recipe__icon">
                   <use href="${icons}#icon-check"></use>
                 </svg>
-                <div class="recipe__quantity">${ing.quantity}</div>
+                <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
                 <div class="recipe__description">
                   <span class="recipe__unit">${ing.unit}</span>
                   ${ing.description}

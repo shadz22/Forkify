95c2f2aa88c352c7c3af09b9c4e1c4db4f72521e
shadz22 <shadz22@yahoo.co.uk>
2021-01-14 18:21:07 +0000

    Create 'recipeView' to move the view code from the controller to the view

--- src/js/model.js
+++ src/js/model.js
@@ -8,7 +8,7 @@ export const loadRecipe = async function(id) {
   try {
     const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`)
     const data = await res.json();
-
+    
     if(!res.ok) throw new Error(`${data.message}(${res.status})`);
     
     const {recipe} = data.data;
@@ -22,7 +22,7 @@ export const loadRecipe = async function(id) {
       image: recipe.image_url,
       ingredients: recipe.ingredients
     };
-  console.log(state.recipe);
+  
   } catch (err) {
     alert(err);
   }

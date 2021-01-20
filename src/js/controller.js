ec53bfd876652d0c6ce39b0b416f99da34714c13
shadz22 <shadz22@yahoo.co.uk>
2021-01-20 16:22:27 +0000

    Add event handler for the search input field

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,6 @@
 import * as model from './model.js'
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js'
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
@@ -22,16 +23,22 @@ const controlRecipes = async function() {
 
 const controlSearchResults = async function() {
   try {
-    await model.loadSearchResults('pizza');
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if(!query) return;
+
+    // 2) Load search results
+    await model.loadSearchResults(query);
+    console.log(model.state.search.results);
 
   } catch(err) {
     console.log(err)
   }
 }
-controlSearchResults();
 
 const init = function() {
 recipeView.addHandlerRender(controlRecipes);
+searchView.addHandlerSearch(controlSearchResults);
 };
 
 init();

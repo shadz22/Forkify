2ee2b994f603c8d74feab175c7ffeb8157bed50e
shadz22 <shadz22@yahoo.co.uk>
2021-01-27 17:28:22 +0000

    Show only 10 search results per page

--- src/js/model.js
+++ src/js/model.js
@@ -1,11 +1,13 @@
 import { async } from 'regenerator-runtime';
-import { API_URL } from './config.js'
+import { API_URL, REC_PER_PAGE } from './config.js'
 import { getJSON } from './helpers.js'
 
 export const state = {
   recipe: {},
   search: {
     query: '',
+    page: 1,
+    resultsPerPage: REC_PER_PAGE,
     results: []
   }
 };
@@ -50,5 +52,13 @@ export const loadSearchResults = async function(query) {
     console.log(err)
     throw err;
   }
+}
+
+export const getSearchResultsPage = function(page = state.search.page) {
+  state.search.page = page;
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
+
+  return state.search.results.slice(start, end);
 }
 

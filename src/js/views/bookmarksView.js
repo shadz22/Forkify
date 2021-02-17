fece3ea425f1b40ea302a0f7b4c1daf5e87339d5
shadz22 <shadz22@yahoo.co.uk>
2021-02-17 11:31:15 +0000

    Refactoring - Create a 'Parent view' to use for both bookmarks and search results

--- src/js/views/bookmarksView.js
+++ src/js/views/bookmarksView.js
@@ -1,5 +1,5 @@
 import View from './View.js';
-import icons from 'url:../../img/icons.svg';
+import previewView from './previewView.js';
 
 class BookmarksView extends View{
   _parentElement = document.querySelector('.bookmarks__list');
@@ -7,24 +7,8 @@ class BookmarksView extends View{
   _message = '';
 
   _generateMarkup() {
-    return this._data.map(this._generateMarkupPreview).join('');
-  }
-
-  _generateMarkupPreview(result) {
-    const id = window.location.hash.slice(1);
-    return `
-      <li class="preview">
-        <a class="preview__link ${result.id === id ? 'preview__link--active' : ''}" href="#${result.id}"> 
-          <figure class="preview__fig">
-            <img src="${result.image}" alt="${result.title}" />
-          </figure>
-          <div class="preview__data">
-            <h4 class="preview__title">${result.title}</h4>
-            <p class="preview__publisher">${result.publisher}</p>
-          </div>
-        </a>
-      </li>
-    `
+    console.log(this._data);
+    return this._data.map(bookmark => previewView.render(bookmark, false)).join('');
   }
 }
 export default new BookmarksView();

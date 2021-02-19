e1a543fe90e240c741537830e0d883751ce24239
shadz22 <shadz22@yahoo.co.uk>
2021-02-19 12:12:24 +0000

    Show and Hide the 'Add Recipe Form'

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,13 +4,12 @@ import searchView from './views/searchView.js'
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
+
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 
-// if(module.hot) {
-//   module.hot.accept();
-// }
 const controlRecipes = async function() {
   try {
     const id = window.location.hash.slice(1);

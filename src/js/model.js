15a2599082628f21f68f12f7e347afface012c1c
shadz22 <shadz22@yahoo.co.uk>
2021-03-03 16:35:28 +0000

    Refactor the helpers' functions

--- src/js/model.js
+++ src/js/model.js
@@ -1,6 +1,6 @@
 import { async } from 'regenerator-runtime';
 import { API_URL, REC_PER_PAGE, KEY } from './config.js'
-import { getJSON, sendJSON } from './helpers.js'
+import { AJAX } from './helpers.js'
 
 export const state = {
   recipe: {},
@@ -31,7 +31,7 @@ const createRecipeObject = function(data) {
 
 export const loadRecipe = async function(id) {
   try {
-    const data = await getJSON(`${API_URL}${id}`);
+    const data = await AJAX(`${API_URL}${id}`);
 
     state.recipe = createRecipeObject(data);
   
@@ -49,7 +49,7 @@ export const loadSearchResults = async function(query) {
   try {
     state.search.query = query;
     
-    const data = await getJSON(`${API_URL}?search=${query}`);
+    const data = await AJAX(`${API_URL}?search=${query}`);
     
     state.search.results = data.data.recipes.map(rec => {
       return {
@@ -134,7 +134,7 @@ export const uploadRecipe = async function(newRecipe) {
       ingredients,
     }
   
-    const data = await sendJSON(`${API_URL}?key=${KEY}`, recipe);
+    const data = await AJAX(`${API_URL}?key=${KEY}`, recipe);
     state.recipe = createRecipeObject(data);
     addBookmarks(state.recipe);
 
